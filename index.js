443d480cce245ec4dbb4624cefc08e4c6a763112
Michael Korostelev <korostelevm@gmail.com>
2022-01-18 12:55:14 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
-var http = require('http');
-http.createServer(function (req, res) {
-    console.log(`Just got a request at ${req.url}!`)
-    res.write('Yo!');
-    res.end();
-}).listen(process.env.PORT || 3000);
+const express = require('express')
+const app = express()
+app.get('/.well-known/acme-challenge/fSCRWNtik5GSdmFLciA0dknDE_a0B0epnXgR_ryb0yA', function(req, res, next) {
+    res.send('fSCRWNtik5GSdmFLciA0dknDE_a0B0epnXgR_ryb0yA.MTQOQK0Xn3fJb5gyz1fkA-ubDenzde3eXrt1mXDP7q8');
+});
+
+app.all('/', (req, res) => {
+    console.log("Just got a request!")
+    res.json(req.headers)
+})
+app.listen(process.env.PORT || 3000)
