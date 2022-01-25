dbaed50e88282a5d2da70cd491fe436cf86cf925
Mike <korostelevm@gmail.com>
2022-01-25 12:21:34 -0500

    send file

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
+const path = require('path')
 const express = require('express')
 const app = express()
 app.get('/.well-known/acme-challenge/fSCRWNtik5GSdmFLciA0dknDE_a0B0epnXgR_ryb0yA', function(req, res, next) {
     res.send('fSCRWNtik5GSdmFLciA0dknDE_a0B0epnXgR_ryb0yA.MTQOQK0Xn3fJb5gyz1fkA-ubDenzde3eXrt1mXDP7q8');
 });
 
+app.all('/file',(req,res)=>{
+    res.sendFile(path.resolve(__dirname, "./image.png"));
+})
 app.all('/', (req, res) => {
     console.log("Just got a request!")
     res.json(req.headers)
