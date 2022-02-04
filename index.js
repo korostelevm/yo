55a96b18247d3f5b142061ab7edabf78c0d4a38a
Michael Korostelev <korostelevm@gmail.com>
2022-02-04 07:39:59 -0500

    Update index.js

--- index.js
+++ index.js
@@ -5,9 +5,20 @@ app.get('/.well-known/acme-challenge/fSCRWNtik5GSdmFLciA0dknDE_a0B0epnXgR_ryb0yA
     res.send('fSCRWNtik5GSdmFLciA0dknDE_a0B0epnXgR_ryb0yA.MTQOQK0Xn3fJb5gyz1fkA-ubDenzde3eXrt1mXDP7q8');
 });
 
+
+function sleep(ms) {
+    return new Promise((resolve) => setTimeout(resolve, ms))
+}
+
+app.get('/sleep/:secs', async (req,res)=>{
+    await sleep(req.params.secs*1000)
+    
+    return res.send(`slept ${req.params.secs}`)
+})
 app.all('/file',(req,res)=>{
     res.sendFile(path.resolve(__dirname, "./image.png"));
 })
+
 app.all('/', (req, res) => {
 //     console.log("Just got a request!")
 //     res.statusCode = 401
