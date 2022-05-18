6d384203bbbad60a058d8961ecb670fe9e554b6d
Mike <korostelevm@gmail.com>
2022-05-18 12:57:46 -0400

    png

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var AWS = require("aws-sdk");
 //   region: process.env.AWS_REGION || "us-east-2",
 // });
 let s3 = new AWS.S3()
-
+const router = express.Router()
 
 process.env.CYCLIC_DB = 'glamorous-battledress-tickCyclicDB'
 
@@ -16,70 +16,82 @@ const CyclicDb = require('cyclic-dynamodb')
 let j = CyclicDb.collection('junk')
 
 
-function sleep(ms) {
-    return new Promise((resolve) => setTimeout(resolve, ms))
-}
-
-app.get('/sleep/:secs', async (req,res)=>{
-    console.log(`sleeping ${req.params.secs}`)
-    for(let i=0; i<=(+req.params.secs); i++){
-        console.log(i)
-        await sleep(1000)
-    }
-    
-    
-    return res.send(`slept ${req.params.secs}`)
-})
-app.get('/s3',async (req,res)=>{
+app.use('/',router)
+router.get('/stats/:uid/:hash',async(req, res)=>{
     let obj = await s3.getObject({
-        Bucket: 'cyclic-glamorous-battledress-tick-us-east-2',
-        Key: 'Screen Shot 2022-05-10 at 3.41.07 PM.png'
-    }).promise()
-    // console.log(obj)
-    res.set('content-type',obj.ContentType)
-    res.send(obj.Body)
+                Bucket: 'cyclic-glamorous-battledress-tick-us-east-2',
+                Key: `${req.params.uid}/${req.params.hash}`
+            }).promise()
+        // console.log(obj)
+        res.set('content-type',obj.ContentType)
+        res.send(obj.Body)
+
 })
 
-app.all('/file',(req,res)=>{
+// function sleep(ms) {
+//     return new Promise((resolve) => setTimeout(resolve, ms))
+// }
+
+// app.get('/sleep/:secs', async (req,res)=>{
+//     console.log(`sleeping ${req.params.secs}`)
+//     for(let i=0; i<=(+req.params.secs); i++){
+//         console.log(i)
+//         await sleep(1000)
+//     }
     
     
-    res.sendFile(path.resolve(__dirname, "./image.png"));
-})
+//     return res.send(`slept ${req.params.secs}`)
+// })
+// app.get('/s3',async (req,res)=>{
+//     let obj = await s3.getObject({
+//         Bucket: 'cyclic-glamorous-battledress-tick-us-east-2',
+//         Key: 'Screen Shot 2022-05-10 at 3.41.07 PM.png'
+//     }).promise()
+//     // console.log(obj)
+//     res.set('content-type',obj.ContentType)
+//     res.send(obj.Body)
+// })
 
-app.get('/error', async (req,res)=>{
-    throw 'error'
-})
-
-app.all('/', async (req, res) => {
-    console.log(process.env)
-//    let last_req = await j.get('last_req')
-//    await j.set('last_req', req.query)
-
-    res.cookie(`c`,Buffer.from('yo').toString('base64'), { 
-    domain: 'cyclic-app.com',
-    maxAge: 900000, httpOnly: true });
+// app.all('/file',(req,res)=>{
     
-    console.log("Just got a request!")
-//     res.statusCode = 401
-//     res.setHeader('WWW-Authenticate','Basic')
-    return res.json({
-        message: 'main bdddranch1'
-    })
-})
+    
+//     res.sendFile(path.resolve(__dirname, "./image.png"));
+// })
 
-app.get('/junk', async (req, res)=>{
-       let last_req = await j.get('last_req')
-       await j.set('last_req', req.headers)
-       let props = last_req.props || {}
-       return res.json({
-           'user-agent': props['user-agent'],
-           updated: props.updated
+// app.get('/error', async (req,res)=>{
+//     throw 'error'
+// })
+
+// app.all('/', async (req, res) => {
+//     console.log(process.env)
+// //    let last_req = await j.get('last_req')
+// //    await j.set('last_req', req.query)
+
+//     res.cookie(`c`,Buffer.from('yo').toString('base64'), { 
+//     domain: 'cyclic-app.com',
+//     maxAge: 900000, httpOnly: true });
+    
+//     console.log("Just got a request!")
+// //     res.statusCode = 401
+// //     res.setHeader('WWW-Authenticate','Basic')
+//     return res.json({
+//         message: 'main bdddranch1'
+//     })
+// })
+
+// app.get('/junk', async (req, res)=>{
+//        let last_req = await j.get('last_req')
+//        await j.set('last_req', req.headers)
+//        let props = last_req.props || {}
+//        return res.json({
+//            'user-agent': props['user-agent'],
+//            updated: props.updated
            
-       })
-})
+//        })
+// })
 
-app.get('/ifttt', (req, res)=>{
-console.log('yyyy')
-res.status(200).send('ok')
-})
+// app.get('/ifttt', (req, res)=>{
+// console.log('yyyy')
+// res.status(200).send('ok')
+// })
 app.listen(3000)
