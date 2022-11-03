00bb68d5270c459d819fc361f03aba145040f049
Mike Korostelev <korostelevm@gmail.com>
2022-11-03 09:24:04 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,10 @@
 const path = require('path')
 const express = require('express')
 const app = express()
-app.get('/.well-known/acme-challenge/fSCRWNtik5GSdmFLciA0dknDE_a0B0epnXgR_ryb0yA', function(req, res, next) {
-    res.send('fSCRWNtik5GSdmFLciA0dknDE_a0B0epnXgR_ryb0yA.MTQOQK0Xn3fJb5gyz1fkA-ubDenzde3eXrt1mXDP7q8');
-});
 
-app.all('/file',(req,res)=>{
-    res.sendFile(path.resolve(__dirname, "./image.png"));
-})
 app.all('/', (req, res) => {
-    console.log("Just got a request!")
+    console.log(Date.now())
+    console.log(req.headers)
     res.json(req.headers)
 })
 app.listen(process.env.PORT || 3000)
