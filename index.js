bc8b659bf68dff5df754569cfc65e5bd5f2cbfd0
Mike Korostelev <korostelevm@gmail.com>
2022-11-03 09:32:22 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 const axios = require('axios')
 
-app.all('*', (req, res) => {
+app.all('*', async (req, res) => {
     console.log('Recieved request')
     let r = await    axios.get(`https://api.techniknews.net/ipgeo/${req.headers['x-forwarded-for']}`)
 
