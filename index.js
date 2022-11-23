ebafa79c54be09d14f120de37281bc9749f04d6b
Mike <korostelevm@gmail.com>
2022-11-23 13:19:29 -0500

    edit menu

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const router = express.Router()
 
-router.get('/*', async (req, res)=>{
+router.get('*', async (req, res)=>{
     console.log(req.path)
     console.log(req.headers)
     console.log("Just got a request!")
